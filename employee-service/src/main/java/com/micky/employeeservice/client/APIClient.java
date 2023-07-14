554c3d739c4d23a28724bf031a7555e58145b8e1
letung999 <letung1112000@gmail.com>
2023-07-14 10:03:01 +0700

    implement code employee service

--- employee-service/src/main/java/com/micky/employeeservice/client/APIClient.java
+++ employee-service/src/main/java/com/micky/employeeservice/client/APIClient.java
@@ -6,7 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+//@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping(value = "department/detail/{departmentCode}")
     ResponseEntity<DepartmentDto> getDepartmentByDepartmentCode(@PathVariable String departmentCode);
